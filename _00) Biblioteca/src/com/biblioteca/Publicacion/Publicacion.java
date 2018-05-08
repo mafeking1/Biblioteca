e9a29b20e848c44e9e6e40a202c85b0a809f8308
Alejandro <mafeking1@hotmail.com>
2018-05-07 23:13:36 -0300

    Primer commit

---
+++ _00) Biblioteca/src/com/biblioteca/Publicacion/Publicacion.java
@@ -0,0 +1,14 @@
+package com.biblioteca.Publicacion;
+
+public abstract class Publicacion {
+
+    private Long codigoIsbn;
+
+    public Long getCodigoIsbn() {
+        return codigoIsbn;
+    }
+
+    public void setCodigoIsbn(Long codigoIsbn) {
+        this.codigoIsbn = codigoIsbn;
+    }
+}
